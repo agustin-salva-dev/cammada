ee76c2ab732668caa4bd3fed79c6e74f38010fdd
AGVS <agvstin.77@gmail.com>
2026-07-16 18:36:14 -0300

    feat(landing): implementación de la landing page y el diseño de navegación.
    - Se ha añadido una sección principal en la página de destino que muestre los detalles del evento «Fight Session n.º 10».
    - Se han integrado componentes visuales personalizados, como una barra de navegación, el estilo del logotipo y una cuenta atrás animada.
    - Se añadieron contenedores de diseño adaptativo, efectos de estilo temáticos para mejorar la interactividad de la interfaz de usuario.

--- components/layout/CammadaLogo.tsx
+++ components/layout/CammadaLogo.tsx
@@ -3,12 +3,14 @@ import { Flame } from "lucide-react";
 export function CammadaLogo() {
   return (
     <div className="relative leading-3.5 flex items-center gap-2 font-heading">
-      <Flame size={21} />
+      <Flame size={21} className="drop-shadow-lg" />
       <div>
-        <h1 className="text-[22px] font-medium tracking-tight">
+        <h1 className="shadow-md text-[22px] font-medium tracking-tight">
           Ca<span className="text-primary">MMA</span>da
         </h1>
-        <h2 className="text-md font-light tracking-tight">Fight Session</h2>
+        <h2 className="shadow-md text-md font-light tracking-tight">
+          Fight Session
+        </h2>
       </div>
     </div>
   );
---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { CammadaLogo } from "./CammadaLogo";
+import { Calendar, Sword, Trophy } from "lucide-react";
+import SpecularButton from "../SpecularButton";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 w-full flex items-center justify-between px-12 py-6">
+      <CammadaLogo />
+      <section className="bg-white/5 backdrop-blur-sm border border-white/10 hover:border-primary/40 rounded-xl drop-shadow-lg flex gap-4 font-normal text-xs text-foreground/50 px-6 py-2 items-center">
+        <div className="group flex gap-2 items-center hover:text-foreground hover:opacity-100 hover:scale-[102%] transition-all duration-200 cursor-pointer">
+          <Calendar size={15} strokeWidth={1} />
+          <p>Eventos</p>
+        </div>
+        <div className="group flex gap-2 items-center hover:text-foreground hover:opacity-100 hover:scale-[102%] transition-all duration-200 cursor-pointer">
+          <Sword size={15} strokeWidth={1} />
+          <p>Combates</p>
+        </div>
+        <div className="group flex gap-2 items-center hover:text-foreground hover:opacity-100 hover:scale-[102%] transition-all duration-200 cursor-pointer">
+          <Trophy size={15} strokeWidth={1} />
+          <p>Rankings</p>
+        </div>
+      </section>
+      <div className="flex items-center gap-4">
+        <SpecularButton
+          size="sm"
+          radius={18}
+          tint="#ffffff"
+          tintOpacity={0}
+          blur={0}
+          textColor="#9f9fa9"
+          lineColor="#ffffff"
+          baseColor="#525252"
+          intensity={1.5}
+          shineSize={12}
+          shineFade={26}
+          thickness={0.8}
+          speed={0.2}
+          followMouse
+          proximity={250}
+          autoAnimate={false}
+          onClick={() => console.log("clicked")}
+          className="hover:text-foreground transition-all transition-200 drop-shadow-md"
+        >
+          Entradas
+        </SpecularButton>
+        <SpecularButton
+          size="sm"
+          radius={18}
+          tint="#c970f5"
+          tintOpacity={0}
+          blur={11}
+          textColor="#581a77"
+          lineColor="#a51fe8"
+          baseColor="#805197"
+          intensity={1.5}
+          shineSize={12}
+          shineFade={26}
+          thickness={0.8}
+          speed={0.2}
+          followMouse
+          proximity={250}
+          autoAnimate={false}
+          onClick={() => console.log("clicked")}
+          className="hover:text-primary transition-all transition-200 drop-shadow-md"
+        >
+          Ver en vivo
+        </SpecularButton>
+      </div>
+    </nav>
+  );
+}
