65675902151d7ccc0f78162ca8f25b46b06ef761
AGVS <agvstin.77@gmail.com>
2026-07-16 18:59:32 -0300

    fix: se ha solucionado un posible error de tipo nulo en SpecularButton

--- components/SpecularButton.tsx
+++ components/SpecularButton.tsx
@@ -269,7 +269,7 @@ const SpecularButton = ({
         p.followMouse &&
         pointerAngle != null &&
         (!p.autoAnimate || proximityT > 0);
-      const target = steer ? pointerAngle : idleAngle;
+      const target = steer && pointerAngle != null ? pointerAngle : idleAngle;
       const diff = ((target - angle + Math.PI * 3) % (Math.PI * 2)) - Math.PI;
       angle += diff * (1 - Math.exp(-dt * 7));
 
