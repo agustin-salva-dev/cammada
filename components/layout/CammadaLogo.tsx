017a0371dcc9f3b9e6dc2077c91c99610707f94c
AGVS <agvstin.77@gmail.com>
2026-07-17 12:38:50 -0300

    feat(ui): se ha optimizado la adaptabilidad a pantallas pequeñas del diseño de la página de inicio y se añadio el botón para activar/desactivar el modo oscuro
    - Se ha integrado el componente ModeToggle en la barra de navegación principal.
    - Se han corregido los problemas de desajuste en la hidratación del componente de fondo Aurora comprobando el estado de montaje.
    - Se han ajustado los estilos adaptativos, las sombras del texto y el espaciado en la página de inicio, la barra de navegación y el pie de página.
    - Se ha encerrado el contenido de la página de inicio dentro de una etiqueta «main» semántica para mejorar el SEO y la accesibilidad.

--- components/layout/CammadaLogo.tsx
+++ components/layout/CammadaLogo.tsx
@@ -5,10 +5,10 @@ export function CammadaLogo() {
     <div className="relative leading-3.5 flex items-center gap-2 font-heading">
       <Flame size={21} className="drop-shadow-lg" />
       <div>
-        <h1 className="shadow-md text-[22px] font-medium tracking-tight">
+        <h1 className="text-shadow-md text-[22px] font-medium tracking-tight">
           Ca<span className="text-primary">MMA</span>da
         </h1>
-        <h2 className="shadow-md text-md font-light tracking-tight">
+        <h2 className="text-shadow-md text-md font-light tracking-tight">
           Fight Session
         </h2>
       </div>
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -3,12 +3,13 @@
 import { CammadaLogo } from "./CammadaLogo";
 import { Calendar, Sword, Trophy } from "lucide-react";
 import SpecularButton from "../SpecularButton";
+import { ModeToggle } from "../ui/ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-8 lg:px-10 py-4 md:py-6 z-50 bg-background/5 backdrop-blur-md md:bg-transparent md:backdrop-blur-none">
+    <nav className="fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-8 lg:px-[42px] 2xl:px-42 py-4 z-50 backdrop-blur-xl bg-transparent lg:backdrop-blur-none">
       <CammadaLogo />
-      <section className="hidden md:flex bg-white/5 backdrop-blur-sm border border-white/10 hover:border-primary/40 rounded-xl drop-shadow-lg gap-4 font-normal text-xs text-foreground/50 px-6 py-2 items-center">
+      <section className="hidden md:flex bg-white/5 backdrop-blur-sm border border-white/10 hover:border-primary/40 rounded-xl drop-shadow-lg gap-4 font-normal text-xs 2xl:text-lg text-foreground/50 px-6 py-2 items-center">
         <div className="group flex gap-2 items-center hover:text-foreground hover:opacity-100 hover:scale-[102%] transition-all duration-200 cursor-pointer">
           <Calendar size={15} strokeWidth={1} />
           <p>Eventos</p>
@@ -23,37 +24,35 @@ export default function Navbar() {
         </div>
       </section>
       <div className="flex items-center gap-2 md:gap-4">
-        <div className="hidden sm:block">
-          <SpecularButton
-            size="sm"
-            radius={18}
-            tint="#ffffff"
-            tintOpacity={0}
-            blur={0}
-            textColor="#9f9fa9"
-            lineColor="#ffffff"
-            baseColor="#525252"
-            intensity={1.5}
-            shineSize={12}
-            shineFade={26}
-            thickness={0.8}
-            speed={0.2}
-            followMouse
-            proximity={250}
-            autoAnimate={false}
-            onClick={() => console.log("clicked")}
-            className="hover:text-foreground transition-all duration-200 drop-shadow-md"
-          >
-            Entradas
-          </SpecularButton>
-        </div>
+        <SpecularButton
+          size="sm"
+          radius={18}
+          tint="#ffffff"
+          tintOpacity={0}
+          blur={0}
+          textColor="fffffff"
+          lineColor="#ffffff"
+          baseColor="#525252"
+          intensity={1.5}
+          shineSize={12}
+          shineFade={26}
+          thickness={0.8}
+          speed={0.2}
+          followMouse
+          proximity={250}
+          autoAnimate={false}
+          onClick={() => console.log("clicked")}
+          className="font-light text-shadow-md hidden md:block"
+        >
+          Entradas
+        </SpecularButton>
         <SpecularButton
           size="sm"
           radius={18}
           tint="#c970f5"
           tintOpacity={0}
           blur={11}
-          textColor="#581a77"
+          textColor="#a51fe8"
           lineColor="#a51fe8"
           baseColor="#805197"
           intensity={1.5}
@@ -65,10 +64,11 @@ export default function Navbar() {
           proximity={250}
           autoAnimate={false}
           onClick={() => console.log("clicked")}
-          className="hover:text-primary transition-all duration-200 drop-shadow-md"
+          className="font-medium text-shadow-md"
         >
           Ver en vivo
         </SpecularButton>
+        <ModeToggle />
       </div>
     </nav>
   );
